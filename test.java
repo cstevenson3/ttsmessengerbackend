f296402ed8a8955e89261cfc7e69cf0ee63b68c8
Cameron Stevenson <cameron.stevenson03@gmail.com>
2019-01-12 18:14:24 +1300

    Added JavaHTTPServer

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
-public class test{
+import java.util.Date;
+
+public class test {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println(new Date());
     }
 }
